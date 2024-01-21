ffa9dbd22854cdd1bcf314d65bb0050c6f474213
Andrew Pecha <andrew.pecha@gmail.com>
2024-01-21 11:46:44 -0600

    add back amount with semicolon

--- ingredient.js
+++ ingredient.js
@@ -2,6 +2,7 @@ class Product {
     constructor(name,uom, amount){
         this.name = name;
         this.uom = uom;
+        this.amount = amount;
     }
 }
 //it's important to export the class at the end of the file
